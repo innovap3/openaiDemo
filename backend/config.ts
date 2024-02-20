e17688d8a242d382fe98f2e6cb1781e1ac1a77d6
Wang <ken.wang@ISTWLAPAPPLE2047.local>
2024-02-20 14:42:25 +0800

    feat: integrate with github loader

--- backend/config.ts
+++ backend/config.ts
@@ -1,4 +1,4 @@
-const projectName = "example-error-app";
-const REPO_PATH = `../${projectName}/src`;
-
-export { projectName, REPO_PATH };
+export const projectName = "example-error-app";
+export const REPO_FRONTEND_PATH = `../${projectName}/cart-app/src`;
+export const REPO_BACKEND_PATH = `../${projectName}/products-server/src`;
+export const REPO_GITHUB_URL = "https://github.com/innovap3/openaiDemo";
